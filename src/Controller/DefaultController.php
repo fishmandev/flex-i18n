1fe9504cccc5f6ca11f70a407847953082933f56
FishmanDev <fishman.code@gmail.com>
2017-11-07 07:23:48 +0200

    Create default controller and action

---
+++ src/Controller/DefaultController.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+
+/**
+ * Class DefaultController
+ * @package App\Controller
+ */
+class DefaultController extends Controller
+{
+    /**
+     * @Route("/", name="index")
+     */
+    public function indexAction()
+    {
+        return $this->render('default/index.html.twig');
+    }
+}
